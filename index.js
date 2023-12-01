a5c76e4cbf1d66c24d34e2dbc0d1d5d4c72d2fb5
lexiama <alexia.mckoy@gmail.com>
2023-12-01 10:20:22 -0500

    updated

--- index.js
+++ index.js
@@ -3,13 +3,23 @@
 window.onload = () => {
   const BtnSubmit = document.getElementById("BtnSubmit");
   const idInput = document.getElementById("idInput");
+  let container = document.querySelector(".container");
 
   BtnSubmit.addEventListener("click", () => {
-    const baseUrl = `https://jsonplaceholder.typicode.com/todos/${idInput.value}`;
+    const idVal = idInput.value;
+    const baseUrl = `https://jsonplaceholder.typicode.com/todos/${idVal || 1}`;
+
+    console.log(baseUrl);
     fetch(baseUrl)
       .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
+      .then((todo) => {
+        //console.log(todo);
+
+        container.innerHTML = `
+            <h2>Title: ${todo.title || "default title"}</h2>
+            <p>Completed: ${todo.completed || false}</p>
+            <p>Id: ${todo.id || "none"}</p>
+        `;
       });
   });
 };
